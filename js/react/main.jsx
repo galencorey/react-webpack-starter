3757a3cb92cfbe694654054b791d557b98222dac
Galen Corey <galen.corey@gmail.com>
2017-01-13 17:16:26 -0500

    first commit

---
+++ js/react/main.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Main extends Component {
+  render(){
+    console.log("im running")
+    return <h1>The React is Rendering!!!</h1>
+  }
+}
+
+
