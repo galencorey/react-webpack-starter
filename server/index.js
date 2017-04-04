0a7db12f8341cfb4ed30f109933896ddfdf322b1
Galen Corey <galen.corey@gmail.com>
2017-04-04 11:07:02 -0400

    set up server and webpack

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {resolve} = require('path');
+const axios = require('axios');
+
+const app = express()
+
+/*Statically serve js files, serve homepage by default */
+app.use(express.static('js'));
+app.get('/', (req, res) => res.sendFile(resolve(__dirname, 'index.html')));
+
+app.listen(process.env.PORT || '1337');
+
